a2606a7441b62b8aa6752ca6398f574a2368758a
Chris Troutner <chris.troutner@gmail.com>
2022-08-04 10:20:33 -0700

    Customized nav menu

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -27,15 +27,15 @@ class NavMenu extends React.Component {
         <Navbar collapseOnSelect expand='xxxl' bg='dark' variant='dark' style={{ paddingRight: '20px' }}>
           <Navbar.Brand href='#home' style={{ paddingLeft: '20px' }}>
             <Image src={Logo} thumbnail width='50' />{' '}
-            BCH Web3 Wallet
+            AVAX-DEX UI
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
           <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='mr-auto' style={{ padding: '25px' }}>
               <Nav.Link href='#' onClick={() => this.handleClickEvent(0)}>BCH</Nav.Link>
-              <Nav.Link href='#' onClick={() => this.handleClickEvent(1)}>Tokens</Nav.Link>
-              <Nav.Link href='#' onClick={() => this.handleClickEvent(2)}>Wallet</Nav.Link>
+              <Nav.Link href='#' onClick={() => this.handleClickEvent(1)}>BCH Tokens</Nav.Link>
+              <Nav.Link href='#' onClick={() => this.handleClickEvent(2)}>BCH Wallet</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
